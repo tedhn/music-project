c88b00d7388061ec4ff5be06c74d406a65b11169
Ted <heinhtetnaing186@gmail.com>
2022-12-02 19:05:04 +0630

    routes comments

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,17 @@ function App() {
 	return (
 		<div className='App'>
 			<h1 className='text-6xl font-bold underline'>Hello world!</h1>
+
+			{/* 
+/ -> home page
+/search
+		/ -> search page
+		/:query -> search page w/ query
+/playlist
+		/ -> playlist page
+		/:id -> playlist page w/ playlist id
+
+ */}
 		</div>
 	);
 }
