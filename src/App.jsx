0d289bdfef77b4000b1b3afbdf2931a4ce10b57a
ngwethandartun <108940247+ngwethandartun@users.noreply.github.com>
2022-12-03 01:50:29 -0700

    install react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Playlist from "./pages/Playlist";
+import Search from "./pages/Search";
+
 
 function App() {
 	return (
-		<div className='App'>
-			<h1 className='text-6xl font-bold underline'>Hello world!</h1>
-		</div>
-	);
+    <>
+      <div className="App">
+        <h1 className="text-6xl font-bold underline">Hello world!</h1>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="Search/:query" element={<Search />} />
+        <Route path="Playlist/:id" element={<Playlist/>} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
