fd2af6e467bbdbf1dadd8029a38299e02000e69f
nyominkhat <108140828+nyominkhat@users.noreply.github.com>
2022-12-04 22:35:29 +0630

    Navbar playlist api

--- src/api.js
+++ src/api.js
@@ -1,12 +1,16 @@
-import axios from 'axios'
+import axios from "axios";
 
-export const loginAccount = async ( code ) => {
-  const resp = await axios
-    .post( "http://localhost:3001/login", {
-      code,
-    } )
-}
+export const loginAccount = async (code) => {
+  const resp = await axios.post("http://localhost:3001/login", {
+    code,
+  });
+};
 
 export const getMyData = () => {
-  return axios.get( 'http://localhost:3001/me' )
-}
+  return axios.get("http://localhost:3001/me");
+};
+
+export const getPlaylist = async () => {
+  const data = await axios.get("http://localhost:3001/playlist");
+  return data.data.items;
+};
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,72 +1,73 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BsPlusCircle, BsSearch } from "react-icons/bs";
 import { RiBankLine } from "react-icons/ri";
 import { HiOutlineQueueList } from "react-icons/hi2";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { getPlaylist } from "@/api";
+import { useState } from "react";
+import { UserContext } from "@/context/UserContext";
 
 const Navbar = () => {
-	console.count("1");
+  const { user } = useContext(UserContext);
 
-	return (
-		<div className='col-span-2 row-span-5 shadow-lg bg-dark'>
-			<div className='flex flex-col items-center w-full h-full gap-4 p-4 pt-6 md:gap-10 sm:gap-8'>
-				<div className='flex flex-col w-4/5 gap-2 '>
-					<Link
-						to='/'
-						className='flex items-center gap-4 px-4 py-2 text-sm font-medium transition-all duration-300 ease-linear rounded-md md:text-base hover:bg-gray hover:text-gold'>
-						<RiBankLine size={22} /> Library
-					</Link>
-					<Link
-						to='/search'
-						className='flex items-center gap-4 px-4 py-2 text-sm font-medium transition-all duration-300 ease-linear rounded-md md:text-base hover:bg-gray hover:text-gold'>
-						<BsSearch size={22} /> Search
-					</Link>
-					<Link
-						to='/playlist'
-						className='flex items-center gap-4 px-4 py-2 text-sm font-medium transition-all duration-300 ease-linear rounded-md md:text-base hover:bg-gray hover:text-gold'>
-						<HiOutlineQueueList size={22} /> My Playlists
-					</Link>
-				</div>
+  const [playlists, setPlaylists] = useState([]);
 
-				<Link className='flex items-center w-4/5 gap-4 px-4 py-2 text-sm font-medium transition-all duration-300 ease-linear rounded-md md:text-base hover:bg-gray hover:text-gold'>
-					<BsPlusCircle size={20} /> Create Playlist
-				</Link>
+  useEffect(() => {
+    playListDataGet();
+  }, [user]);
 
-				<div className='flex flex-col w-4/5 gap-2 overflow-auto'>
-					<Link
-						to='/playlist/id'
-						className='px-4 py-2 text-sm font-medium transition-all duration-300 ease-linear rounded-md md:text-base hover:bg-gray hover:text-gold'>
-						Playlist Name
-					</Link>
-					<Link
-						to='/playlist/id'
-						className='px-4 py-2 text-sm font-medium transition-all duration-300 ease-linear rounded-md md:text-base hover:bg-gray hover:text-gold'>
-						Playlist Name
-					</Link>
-					<Link
-						to='/playlist/id'
-						className='px-4 py-2 text-sm font-medium transition-all duration-300 ease-linear rounded-md md:text-base hover:bg-gray hover:text-gold'>
-						Playlist Name
-					</Link>
-					<Link
-						to='/playlist/id'
-						className='px-4 py-2 text-sm font-medium transition-all duration-300 ease-linear rounded-md md:text-base hover:bg-gray hover:text-gold'>
-						Playlist Name
-					</Link>
-					<Link
-						to='/playlist/id'
-						className='px-4 py-2 text-sm font-medium transition-all duration-300 ease-linear rounded-md md:text-base hover:bg-gray hover:text-gold'>
-						Playlist Name
-					</Link>
-					<Link
-						to='/playlist/id'
-						className='px-4 py-2 text-sm font-medium transition-all duration-300 ease-linear rounded-md md:text-base hover:bg-gray hover:text-gold'>
-						Playlist Name
-					</Link>
-				</div>
-			</div>
-		</div>
-	);
+  const playListDataGet = async () => {
+    const playlistData = await getPlaylist();
+    setPlaylists(playlistData);
+    console.log(playlistData);
+  };
+
+  console.count("1");
+
+  return (
+    <div className="col-span-2 row-span-5 shadow-lg bg-dark">
+      <div className="flex flex-col items-center w-full h-full gap-4 p-4 pt-6 md:gap-10 sm:gap-8">
+        <div className="flex flex-col w-4/5 gap-2 ">
+          <Link
+            to="/"
+            className="flex items-center gap-4 px-4 py-2 text-sm font-medium transition-all duration-300 ease-linear rounded-md md:text-base hover:bg-gray hover:text-gold"
+          >
+            <RiBankLine size={22} /> Library
+          </Link>
+          <Link
+            to="/search"
+            className="flex items-center gap-4 px-4 py-2 text-sm font-medium transition-all duration-300 ease-linear rounded-md md:text-base hover:bg-gray hover:text-gold"
+          >
+            <BsSearch size={22} /> Search
+          </Link>
+          <Link
+            to="/playlist"
+            className="flex items-center gap-4 px-4 py-2 text-sm font-medium transition-all duration-300 ease-linear rounded-md md:text-base hover:bg-gray hover:text-gold"
+          >
+            <HiOutlineQueueList size={22} /> My Playlists
+          </Link>
+        </div>
+
+        <Link className="flex items-center w-4/5 gap-4 px-4 py-2 text-sm font-medium transition-all duration-300 ease-linear rounded-md md:text-base hover:bg-gray hover:text-gold">
+          <BsPlusCircle size={20} /> Create Playlist
+        </Link>
+
+        {/* playList names */}
+        <div className="flex flex-col w-4/5 gap-2 overflow-auto">
+          {playlists.map((playlist) => (
+            <Link
+              key={playlist.id}
+              to={`/playlist/${playlist.id}`}
+              className="px-4 py-2 text-sm font-medium transition-all duration-300 ease-linear rounded-md md:text-base hover:bg-gray hover:text-gold"
+            >
+              {playlist.name}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
