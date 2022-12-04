22a96ba0722c04a9fc9391644597316dfb6d3710
Ted <heinhtetnaing186@gmail.com>
2022-12-04 20:51:13 +0630

    updated playlist component ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,14 @@
 import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <h1 className="font-bold text-7xl leading-normal text-white">
-        My Libiary
-      </h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className='font-bold text-7xl leading-normal text-white'>
+				My Libiary
+			</h1>
+		</div>
+	);
 };
 
 export default Home;
 //for playlist name page//
-//  <div className=" flex gap-5">
-//    <div className="w-40 h-40 drop-shadow-xl">
-//      <img  src="" alt="image" />
-//    </div>
-//    <div>
-//      <h1 className="font-bold text-7xl leading-normal text-white">
-//        Playlist Name
-//      </h1>
-//      <ul className="flex gap-3">
-//        <li>Playlist owner</li>
-//        <li>Total songs</li>
-//      </ul>
-//    </div>
-//  </div>;
