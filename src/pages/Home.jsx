ab4b48d588b1435c1d86b2889196d60ad96f35d8
leonlynn1001 <109093697+leonlynn1001@users.noreply.github.com>
2022-12-05 01:24:28 +0630

    mylibrary

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ListItem from "../components/ListItem";
-import Card from "../components/Card";
+
 const Home = () => {
   return (
     <div className="w-full">
-      <h1>My Libaray</h1>
-      <div className=" ml-8 w-11/12 h-16 px-8 py-8 flex flex-row  items-center gap-64 bg-gray  text-white">
-        <p># Title</p>
-        <p>Album</p>
-        <p>Date Added</p>
-        <p>Duration</p>
+      <h1 className=" py-8 text-3xl text-offWhite font-black ml-8">
+        My Libaray
+      </h1>
+      <div className=" ml-8 w-11/12  flex flex-row  items-arround gap-24 bg-gray  text-white">
+        <p className="pl-4 pr-4">#No</p>
+        <p className="pl-4 pr-4">Title</p>
+        <p className=" text-center pl-4 pr-4 mr-1">Album</p>
+        <p className="pl-4 pr-4">Date Added</p>
+        <p className="pr-4 pl-4">Duration</p>
       </div>
+      <hr className="w-11/12  ml-8"></hr>
       <ListItem />
     </div>
   );
