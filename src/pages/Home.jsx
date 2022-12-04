bd53e240d028bed74bffab646bc3be5590cc0505
ngwethandartun <108940247+ngwethandartun@users.noreply.github.com>
2022-12-04 00:55:12 -0700

    add-layout ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Home = () => {
-	return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="font-bold text-7xl leading-normal text-white">
+        My Libiary
+      </h1>
+    </div>
+  );
 };
 
 export default Home;
+//for playlist name page//
+//  <div className=" flex gap-5">
+//    <div className="w-40 h-40 drop-shadow-xl">
+//      <img  src="" alt="image" />
+//    </div>
+//    <div>
+//      <h1 className="font-bold text-7xl leading-normal text-white">
+//        Playlist Name
+//      </h1>
+//      <ul className="flex gap-3">
+//        <li>Playlist owner</li>
+//        <li>Total songs</li>
+//      </ul>
+//    </div>
+//  </div>;
