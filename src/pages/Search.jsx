6e119655fcc18f005fdf5c91b00d86d5649a5b72
nyominkhat <108140828+nyominkhat@users.noreply.github.com>
2022-12-05 23:02:42 +0630

    restyling Search component

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,26 +1,80 @@
 import React from "react";
+import { BsSearch } from "react-icons/bs";
 
 import { ListItem, Card } from "../components";
 
 const Search = () => {
   return (
-    <div>
-      <input
-        className="w-64 p-1 mt-1 ml-8 rounded-md shadow-xl text-dark bg-offwhite"
-        value="Search"
-      ></input>
+    <div className="flex  flex-col justify-between h-full ">
+      <div className="relative rounded-md  md:w-96">
+        <BsSearch className="absolute ml-3 text-black textbo top-3" />
+        <input
+          className="w-full  px-4 py-2 pl-10 border-none rounded-md outline-none text-dark shadow-xl bg-offwhite"
+          // value="Search"
+        ></input>
+      </div>
 
-      <div>Playlist</div>
-      <div className="w-4/5 mx-auto">
+      <div className="px-4 my-5  custom-scrollbars__content h-5/6">
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
+        <ListItem />
         <ListItem />
         <ListItem />
         <ListItem />
         <ListItem />
       </div>
 
-      <div>Albums</div>
+      <h3 className="my-5 text-3xl font-semibold">Albums</h3>
 
-      <div className="w-4/5 gap-4 mx-auto flex justify-start flex-wrap">
+      <div className="flex  flex-wrap px-12 md:justify-start justify-center w-full gap-4  custom-scrollbars__content h-2/5">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
         <Card />
         <Card />
         <Card />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'black': '#000000',
-      'dark': '#222831',
-      'gray': '#393E46',
-      'offwhite': '#EEEEEE',
-      'gold': '#FFD369',
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "#000000",
+      dark: "#222831",
+      gray: "#393E46",
+      offwhite: "#EEEEEE",
+      gold: "#FFD369",
     },
   },
   plugins: [],
-}
+};
