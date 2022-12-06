1fa6f4cb052d69f164a73757f0ec8f9688c2c997
Ted <heinhtetnaing186@gmail.com>
2022-12-06 20:54:48 +0630

    removed consolelogs in nav

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -5,16 +5,16 @@ import { ListItem, Card } from "../components";
 
 const Search = () => {
 	return (
-		<div className='flex  flex-col justify-between h-full '>
-			<div className='relative rounded-md  md:w-96'>
+		<div className='flex flex-col justify-between h-full '>
+			<div className='relative rounded-md md:w-96'>
 				<BsSearch className='absolute ml-3 text-black textbo top-3' />
 				<input
-					className='w-full  px-4 py-2 pl-10 border-none rounded-md outline-none text-dark shadow-xl bg-offwhite'
+					className='w-full px-4 py-2 pl-10 border-none rounded-md shadow-xl outline-none text-dark bg-offwhite'
 					// value="Search"
 				></input>
 			</div>
 
-			<div className='px-4 my-5  custom-scrollbars__content h-5/6'>
+			<div className='px-4 my-5 custom-scrollbars__content h-5/6'>
 				<ListItem />
 				<ListItem />
 				<ListItem />
@@ -59,7 +59,7 @@ const Search = () => {
 
 			<h3 className='my-5 text-3xl font-semibold'>Albums</h3>
 
-			<div className='flex  flex-wrap px-12 md:justify-start justify-center w-full gap-4  custom-scrollbars__content h-2/5'>
+			<div className='flex flex-wrap justify-center w-full gap-4 px-12 md:justify-start custom-scrollbars__content h-2/5'>
 				<Card />
 				<Card />
 				<Card />
@@ -89,3 +89,5 @@ const Search = () => {
 		</div>
 	);
 };
+
+export default Search;
