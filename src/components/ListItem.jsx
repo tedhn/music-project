ff58e3b60c29ba3e15d549dcad85e8a0b5776241
Ted <heinhtetnaing186@gmail.com>
2022-12-06 20:53:55 +0630

    updated listitem styling

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -4,28 +4,26 @@ import { Link } from "react-router-dom";
 const ListItem = () => {
 	return (
 		// listSection
-		<div className='hover:rounded-lg  w-11/12 h-16 px-8 py-8 flex flex-row  items-center gap-20 bg-gray hover:bg-offwhite text-white hover:text-gold'>
+		<div className='grid items-center justify-center grid-cols-12 text-white hover:rounded-lg bg-gray hover:bg-offwhite/20 hover:text-gold'>
 			{/* listItemLeft */}
-			<div className='flex flex-row justify-center items-center gap-8'>
-				<p>1</p>
-				<img
-					src='https://preview.redd.it/m2g632yc33171.jpg?width=640&crop=smart&auto=webp&s=4d766d90b83b2b97d5d874bcf01d86bbabc8131c'
-					alt='artist'
-					// singerPic
-					className='w-10 h-10'
-				/>
-				<div>
-					<p>Song Title</p>
-					<small>Artist Title</small>
-				</div>
+
+			<p className='mx-4'>1</p>
+			<img
+				src='https://preview.redd.it/m2g632yc33171.jpg?width=640&crop=smart&auto=webp&s=4d766d90b83b2b97d5d874bcf01d86bbabc8131c'
+				alt='artist'
+				// singerPic
+				className='w-10 h-10'
+			/>
+			<div className='col-span-5 text-start'>
+				<p>Song Title</p>
+				<small>Artist Title</small>
 			</div>
 			{/* listItemData */}
-			<div className='flex flex-row items-center justify-evenly gap-32'>
-				<p>Album Name</p>
-				<p>Date Added</p>
-				<p>Duration</p>
-				<p>...</p>
-			</div>
+
+			<p className='col-span-2 '>Album Name</p>
+			<p>Date Added</p>
+			<p>Duration</p>
+			<p>...</p>
 		</div>
 	);
 };
