a6136059bc22c4388552aa20658f26597c0fa733
Ted <heinhtetnaing186@gmail.com>
2022-12-06 19:07:00 +0630

    Revert "Merge branch 'newcomponents'"
    This reverts commit b837aa8d928ed40e275f62068e3ac3156d3f8ed8, reversing
    changes made to 3d9a71559d616b2dd50660babaedd448b09032fa.

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,32 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 // import "./listItem.css";
 const ListItem = () => {
-  return (
-    // listSection
-    <div className="flex flex-row items-center justify-between w-full h-16 px-8 py-8 text-white transition-all duration-300 ease-linear hover:rounded-lg bg-gray hover:bg-offwhite/20 hover:text-gold">
-      {/* listItemLeft */}
-      <div className="flex flex-row items-center justify-center w-1/3 gap-8">
-        <p>1</p>
-        <img
-          src="https://preview.redd.it/m2g632yc33171.jpg?width=640&crop=smart&auto=webp&s=4d766d90b83b2b97d5d874bcf01d86bbabc8131c"
-          alt="artist"
-          // singerPic
-          className="w-10 h-10"
-        />
-        <div className="grow">
-          <p>Song Title</p>
-          <small>Artist Title</small>
-        </div>
-      </div>
-      {/* listItemData */}
-      <div className="flex flex-row items-center justify-around w-2/3 gap-8">
-        <p>Album Name</p>
-        <p>Date Added</p>
-        <p>Duration</p>
-        <p>...</p>
-      </div>
-    </div>
-  );
+	return (
+		// listSection
+		<div className='hover:rounded-lg  w-11/12 h-16 px-8 py-8 flex flex-row  items-center gap-20 bg-gray hover:bg-offwhite text-white hover:text-gold'>
+			{/* listItemLeft */}
+			<div className='flex flex-row justify-center items-center gap-8'>
+				<p>1</p>
+				<img
+					src='https://preview.redd.it/m2g632yc33171.jpg?width=640&crop=smart&auto=webp&s=4d766d90b83b2b97d5d874bcf01d86bbabc8131c'
+					alt='artist'
+					// singerPic
+					className='w-10 h-10'
+				/>
+				<div>
+					<p>Song Title</p>
+					<small>Artist Title</small>
+				</div>
+			</div>
+			{/* listItemData */}
+			<div className='flex flex-row items-center justify-evenly gap-32'>
+				<p>Album Name</p>
+				<p>Date Added</p>
+				<p>Duration</p>
+				<p>...</p>
+			</div>
+		</div>
+	);
 };
 
 export default ListItem;
