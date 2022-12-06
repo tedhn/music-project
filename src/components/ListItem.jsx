0f7c4952ce52f55d8126174d62cde32f218b27fe
nyominkhat <108140828+nyominkhat@users.noreply.github.com>
2022-12-06 23:54:23 +0630

    SearchPage complete dynamic data

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,33 +1,49 @@
 import React from "react";
-import { Link } from "react-router-dom";
-// import "./listItem.css";
-const ListItem = () => {
-	return (
-		// listSection
-		<div className='hover:rounded-lg  w-11/12 h-16 px-8 py-8 flex flex-row  items-center gap-20 bg-gray hover:bg-offwhite text-white hover:text-gold'>
-			{/* listItemLeft */}
-			<div className='flex flex-row justify-center items-center gap-8'>
-				<p>1</p>
-				<img
-					src='https://preview.redd.it/m2g632yc33171.jpg?width=640&crop=smart&auto=webp&s=4d766d90b83b2b97d5d874bcf01d86bbabc8131c'
-					alt='artist'
-					// singerPic
-					className='w-10 h-10'
-				/>
-				<div>
-					<p>Song Title</p>
-					<small>Artist Title</small>
-				</div>
-			</div>
-			{/* listItemData */}
-			<div className='flex flex-row items-center justify-evenly gap-32'>
-				<p>Album Name</p>
-				<p>Date Added</p>
-				<p>Duration</p>
-				<p>...</p>
-			</div>
-		</div>
-	);
+
+const ListItem = ({
+  songName,
+  artistName,
+  albumName,
+  releasedDate,
+  duration,
+  img,
+  index,
+}) => {
+  function dura(milliseconds) {
+    let seconds = Math.floor(milliseconds / 1000);
+    let minutes = Math.floor(seconds / 60);
+
+    seconds = seconds % 60;
+
+    return `${minutes}:${seconds}`;
+  }
+
+  return (
+    // listSection
+    <div className="grid items-center justify-center grid-cols-12 hover:rounded-lg bg-gray hover:bg-offwhite/20 hover:text-gold hover:cursor-pointer">
+      {/* listItemLeft */}
+      <div className="flex items-center justify-around">
+        <p className="mx-4">{index}</p>
+        <img
+          src={img}
+          alt="artist"
+          // singerPic
+          className="w-10 h-10"
+        />
+      </div>
+      <div className="col-span-5">
+        <p>{songName}</p>
+        <small>{artistName}</small>
+      </div>
+
+      {/* listItemData */}
+
+      <p className="col-span-3">{albumName}</p>
+      <p className="text-center">{releasedDate}</p>
+      <p className="text-center">{dura(duration)}</p>
+      <p className="text-center">...</p>
+    </div>
+  );
 };
 
 export default ListItem;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,91 +1,75 @@
 import React from "react";
 import { BsSearch } from "react-icons/bs";
+import { useState, useEffect, useContext } from "react";
 
 import { ListItem, Card } from "../components";
+import { handleSearch } from "@/api";
 
 const Search = () => {
-	return (
-		<div className='flex  flex-col justify-between h-full '>
-			<div className='relative rounded-md  md:w-96'>
-				<BsSearch className='absolute ml-3 text-black textbo top-3' />
-				<input
-					className='w-full  px-4 py-2 pl-10 border-none rounded-md outline-none text-dark shadow-xl bg-offwhite'
-					// value="Search"
-				></input>
-			</div>
+  const [searchData, setSearchData] = useState("");
+  const [playlists, setPlaylists] = useState([]);
+  const [tracks, setTracks] = useState([]);
 
-			<div className='px-4 my-5  custom-scrollbars__content h-5/6'>
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-				<ListItem />
-			</div>
+  useEffect(() => {
+    const getData = setTimeout(async () => {
+      if (searchData !== "") {
+        searchPlayers(searchData);
+      }
+    }, 1000);
+    return () => clearTimeout(getData);
+  }, [searchData]);
 
-			<h3 className='my-5 text-3xl font-semibold'>Albums</h3>
+  const searchPlayers = async () => {
+    const { playlists, tracks } = await handleSearch(searchData);
 
-			<div className='flex  flex-wrap px-12 md:justify-start justify-center w-full gap-4  custom-scrollbars__content h-2/5'>
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-			</div>
-		</div>
-	);
+    setPlaylists(playlists.items);
+    setTracks(tracks.items);
+    // console.log("tracks", tracks);
+  };
+  // console.log("playlists", playlists);
+  console.log("tracks", tracks);
+
+  return (
+    <div className="flex  flex-col justify-between h-full ">
+      <div className="relative rounded-md  md:w-96">
+        <BsSearch className="absolute ml-3 text-black textbo top-3" />
+        <input
+          className="w-full  px-4 py-2 pl-10 border-none rounded-md outline-none text-dark shadow-xl bg-offwhite"
+          // value="Search"
+          value={searchData}
+          onChange={(e) => setSearchData(e.target.value)}
+        ></input>
+      </div>
+
+      <div className="px-4 my-5  custom-scrollbars__content h-5/6">
+        {tracks.map((track, index) => (
+          <ListItem
+            key={track.id}
+            index={index + 1}
+            songName={track.name}
+            artistName={track.artists[0].name}
+            albumName={track.album.name}
+            releasedDate={track.album.release_date}
+            duration={track.duration_ms}
+            img={track.album.images[0].url}
+          />
+        ))}
+      </div>
+
+      <h3 className="my-5 text-3xl font-semibold">Albums</h3>
+
+      <div className="flex  flex-wrap px-12 md:justify-start justify-center w-full gap-4  custom-scrollbars__content h-2/5">
+        {playlists.map((playlist) => (
+          <Card
+            key={playlist.id}
+            img={playlist.images[0].url}
+            name={playlist.name}
+            ownerName={playlist.owner.display_name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
+
+export default Search;
